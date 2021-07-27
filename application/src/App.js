2e9d78dcc65be47e29b49d6854a8e3da504b85ed
Devon Dy-Liacco <dreydee33@gmail.com>
2021-07-27 13:52:07 -0700

    Implement visual board, fix valid moves

--- application/src/App.js
+++ application/src/App.js
@@ -1,11 +1,174 @@
 import { Client } from 'boardgame.io/client';
+import { Local } from 'boardgame.io/multiplayer';
 import { Totems } from './Game';
 
 class TotemsClient {
-    constructor() {
-        this.client = Client({ game: Totems });
+    constructor(rootElement, { playerID }) {
+        this.client = Client({
+            game: Totems,
+            multiplayer: Local(),
+            playerID
+        });
         this.client.start();
+        this.rootElement = rootElement;
+        this.createBoard();
+        this.attachListeners();
+        this.client.subscribe(state => this.update(state));
+    }
+
+    createBoard() {
+        let main = document.createElement("div");
+        main.className = "main";
+        //board
+        let board = document.createElement("div");
+        board.className = "board";
+        let div;
+        for(let i = 0;i < 100;i++) {
+            div = document.createElement("div");
+            div.className = "square";
+            div.dataset.id = i;
+
+            board.appendChild(div);
+        }
+        main.appendChild(board);
+        //stack
+        let stack = document.createElement("div");
+        stack.className = "stack";
+
+        let confirmButton = document.createElement("div");
+        confirmButton.id = "confirm-button";
+        confirmButton.textContent = "Confirm";
+
+        let cancelButton = document.createElement("div");
+        cancelButton.id = "cancel-button";
+        cancelButton.textContent = "Cancel";
+
+        stack.appendChild(confirmButton);
+        stack.appendChild(cancelButton);
+
+        main.appendChild(stack);
+
+        this.rootElement.appendChild(main);
+        // const rows = [];
+        // for(let i = 0;i < 10;i++) {
+        //     const squares = [];
+        //     for(let j = 0;j < 10;j++) {
+        //         const id = 10 * i + j;
+        //         div = document.createElement("div");
+        //         div.className = "square";
+        //         div.dataset.id = id;
+        //         squares.push(`<td class ="square" data-id="${id}"><td>`);
+        //     }
+        //     rows.push(`<tr class="board">${squares.join('')}</tr>`);
+        // }
+
+        // this.rootElement.innerHTML = `
+        //     <table class="board">${rows.join('')}<table>
+        // `;
+    }
+
+    attachListeners() {
+        const handleSquareClick = event => {
+            const id = parseInt(event.target.dataset.id);
+            this.client.moves.clickSquare(id);
+        };
+        const squares = this.rootElement.querySelectorAll('.square');
+        squares.forEach(square => {
+            square.onclick = handleSquareClick;
+        });
+        const handleConfirmClick = () => {
+            this.client.moves.confirm();
+        };
+        const confirmButton = this.rootElement.querySelector('#confirm-button');
+        confirmButton.onclick = handleConfirmClick;
+        const handleCancelClick = () => {
+            this.client.moves.cancel();
+        };
+        const cancelButton = this.rootElement.querySelector('#cancel-button');
+        cancelButton.onclick = handleCancelClick;
+    }
+
+    update(state) {
+        const confirmButton = this.rootElement.querySelector('#confirm-button');
+        confirmButton.style.display = "none";
+        const cancelButton = this.rootElement.querySelector('#cancel-button');
+        cancelButton.style.display = "none";
+        if(state.G.activePiece.player >= 0) {
+            confirmButton.style.display = "block";
+        }
+        if(state.G.stage == "Move piece") {
+            cancelButton.style.display = "block";
+        }
+        const squares = this.rootElement.querySelectorAll('.square');
+        if(this.client.playerID == 0 && state.ctx.currentPlayer == 0) {
+            squares.forEach(square => {
+                const squareID = parseInt(square.dataset.id);
+                const squareValue = state.G.squares[squareID].pole[state.G.squares[squareID].pole.length-1].type;
+                const squareColor = state.G.squares[squareID].pole[state.G.squares[squareID].pole.length-1].player;
+                square.textContent = squareValue;
+                square.style.color = squareColor < 0 ? "#f0f0f0" : (squareColor == 0 ? "red" : "blue");
+                if(state.G.squares[squareID].side < 0) {
+                    if(state.G.activeSquare == squareID) {
+                        square.style.backgroundColor = "#80d072";
+                    } else {
+                        square.style.backgroundColor = "#797";
+                    }
+                } else if(state.G.squares[squareID].side == 0) {
+                    if(state.G.activeSquare == squareID) {
+                        square.style.backgroundColor = "#ff9292";
+                    } else {
+                        square.style.backgroundColor = "#999292";
+                    }
+                } else {
+                    if(state.G.activeSquare == squareID) {
+                        square.style.backgroundColor = "#9992f0";
+                    } else {
+                        square.style.backgroundColor = "#929299";
+                    }
+                }
+                if(state.G.squares[squareID].valid) {
+                    square.style.border = "1px solid white";
+                } else {
+                    square.style.border = "1px solid green";
+                }
+            });
+        } else if(this.client.playerID == 1 && state.ctx.currentPlayer == 1) {
+            let oppSquares = [];
+            for(let i = 0;i < 100;i++) {
+                oppSquares.push(state.G.squares[99-i]);
+            }
+            squares.forEach(square => {
+                const squareID = parseInt(square.dataset.id);
+                const squareValue = state.G.squares[squareID].pole[state.G.squares[squareID].pole.length-1].type;
+                square.textContent = squareValue;
+                if(state.G.squares[squareID].side < 0) {
+                    if(state.G.activeSquare == squareID) {
+                        square.style.backgroundColor = "#72d080";
+                    } else {
+                        square.style.backgroundColor = "#797";
+                    }
+                } else if(state.G.squares[squareID].side == 0) {
+                    if(state.G.activeSquare == squareID) {
+                        square.style.backgroundColor = "#f09299";
+                    } else {
+                        square.style.backgroundColor = "#999292";
+                    }
+                } else {
+                    if(state.G.activeSquare == squareID) {
+                        square.style.backgroundColor = "#9292ff";
+                    } else {
+                        square.style.backgroundColor = "#929299";
+                    }
+                }
+            });
+        }
     }
 }
 
-const app = new TotemsClient();
+const appElement = document.getElementById('app');
+const playerIDs = ['0', '1'];
+const clients = playerIDs.map(playerID => {
+  const rootElement = document.createElement('div');
+  appElement.append(rootElement);
+  return new TotemsClient(rootElement, { playerID });
+});
--- application/src/Game.js
+++ application/src/Game.js
@@ -13,64 +13,238 @@ function createStartingArray() {
         "", "C", "", "", "", "B", "V", "", "S", "H",
         "", "", "", "A", "A", "A", "A", "V", "H", "S"
     ];
+    const colorSetup = [
+        "-1", "1", "1", "1", "1", "1", "1", "-1", "-1", "-1",
+        "0", "-1", "-1", "1", "1", "-1", "-1", "-1", "1", "-1",
+        "0", "-1", "-1", "-1", "1", "1", "-1", "1", "-1", "-1",
+        "0", "0", "-1", "-1", "-1", "1", "1", "-1", "-1", "1",
+        "0", "0", "0", "-1", "-1", "-1", "1", "1", "-1", "1",
+        "0", "-1", "0", "0", "-1", "-1", "-1", "1", "1", "1",
+        "0", "-1", "-1", "0", "0", "-1", "-1", "-1", "1", "1",
+        "-1", "-1", "0", "-1", "0", "0", "-1", "-1", "-1", "1",
+        "-1", "0", "-1", "-1", "-1", "0", "0", "-1", "-1", "1",
+        "-1", "-1", "-1", "0", "0", "0", "0", "0", "0", "-1"
+    ];
     const setup = Array(100).fill()
-    .map(() => ({
-        pole: [],
-        valid: false,
-        winning: false
-    }))
-    .map(i => {
-        setup[i].push(boardSetup[i]);
-    });
+        .map(() => ({
+            pole: [],
+            side: -1,
+            valid: false,
+            winning: false
+        }));
+    for (let i = 0; i < 100; i++) {
+        setup[i].pole.push({
+            player: colorSetup[i],
+            type: boardSetup[i]
+        });
+    }
+    let redCoords;
+    let blueCoords;
+    for (let i = 0; i < 10; i++) {
+        for(let j = 0;j < i;j++) {
+            blueCoords = { x: i, y: j };
+            setup[toIndex(blueCoords)].side = 1;
+            redCoords = { x: 10-i-1, y: 10-(j+1) };
+            setup[toIndex(redCoords)].side = 0;
+        }
+    }
+
+    //for() set players
 
     return setup;
+}
+
+function setActivePiece(G, ctx, square, height, type, load = { type: "", player: -1 }) {
+    G.activePiece.square = square;
+    G.activePiece.height = height;
+    G.activePiece.type = type;
+    G.activePiece.player = ctx.currentPlayer;
+    G.activePiece.load = load;
+}
+
+function resetActivePiece(G) {
+    G.activePiece.square = -1;
+    G.activePiece.height = -1;
+    G.activePiece.type = "";
+    G.activePiece.player = -1;
+    G.activePiece.load.type = "";
+    G.activePiece.load.player = -1;
+}
+
+function getCoordinates(index) {
+    let xCoord;
+    let yCoord;
+    xCoord = index % 10;
+    yCoord = Math.floor(index / 10);
+    return { x: xCoord, y: yCoord };
+}
+
+function toIndex(coords) {
+    return 10 * coords.y + coords.x;
+}
+
+function offset(coords, dx, dy) {
+    return { x: coords.x + dx, y: coords.y + dy };
+}
+
+function onBoard(coords) {
+    return coords.x >= 0 && coords.x < 10 && coords.y >= 0 && coords.y < 10;
+}
+
+
+function markValidMovesByStep(G, index, heightInd, steps) {
+    if (steps <= 0) {
+        return;
+    }
+    let newSteps = steps - 1;
+    let coords = getCoordinates(index);
+    let tempCoords;
+    for (let i = 0; i < 3; i++) {
+        let dy = i-1;
+        for (let j = 0; j < 3; j++) {
+            let dx = j-1;
+            tempCoords = offset(coords, dx, dy);
+            if (Math.abs(dx) != Math.abs(dy) && onBoard(tempCoords)) {
+                let newIndex = toIndex(tempCoords);
+                if (G.squares[newIndex].pole[G.squares[newIndex].pole.length - 1].type != "H") {
+                    let heightDifference = heightInd - (G.squares[newIndex].pole.length - 1);           //0,1,2,3,4 height = 5
+                    if (heightDifference <= 1 && heightDifference >= -1) {                             //0,1,2 height = 3
+                        G.squares[newIndex].valid = true;                                           //0,1,2,3 height = 4
+                        markValidMovesByStep(G, newIndex, G.squares[newIndex].pole.length, newSteps); //0,1,2,3 height = 3
+                    }
+                }
+            }
+            dx++;
+        }
+        dy++;
+    }
+}
+
+function markValidMovesByFlight(G, index, moves) {
+    if (moves <= 0) {
+        return;
+    }
+    let newMoves = moves - 1;
+    let coords = getCoordinates(index);
+    let tempCoords;
+    for (let i = 0; i < 3; i++) {
+        let dy = i-1;
+        for (let j = 0; j < 3; j++) {
+            let dx = j-1;
+            tempCoords = offset(coords, dx, dy);
+            if (Math.abs(dx) != Math.abs(dy) && onBoard(tempCoords)) {
+                let newIndex = toIndex(tempCoords);
+                if (G.squares[newIndex].pole[G.squares[newIndex].pole.length - 1].type != "H") {
+                    G.squares[newIndex].valid = true;
+                    markValidMovesByFlight(G, newIndex, newMoves);
+                }
+            }
+        }
+    }
+}
+
+function markValidMoves(G, ctx) {
+    // resetValidMoves(G);
+    let index = G.activePiece.square;
+    let height = G.activePiece.height;
+    if (G.activePiece.type == "W" && G.activePiece.load.player >= 0) {
+        height--;
+    }
+    if (height < G.squares[index].length - 1) {
+        return;
+    }
+    if (G.activePiece.type == "V"
+        || G.activePiece.type == "C"
+        || G.activePiece == "F"
+        || G.activePiece == "A"
+        || G.activePiece == "H") {
+        markValidMovesByStep(G, index, height, 2);
+    } else if (G.activePiece.type == "E" || G.activePiece.type == "W") {
+        // if (G.activePiece.type == "W" && G.activePiece.load.player >= 0) {
+        //     height--;
+        // }
+        markValidMovesByStep(G, index, height, 3);
+    } else if (G.activePiece.type == "B") {
+        markValidMovesByFlight(G, index, 4);
+    }
+}
+
+function resetValidMoves(G) {
+    for (let i = 0; i < 100; i++) {
+        G.squares[i].valid = false;
+    }
 }
 
 export const Totems = {
     setup: () => ({
         squares: createStartingArray(),
         activeSquare: -1,
-        activePiece: { square: -1, type: "", player: -1 },
-        stage: "Select a square"
+        activePiece: {
+            square: -1,
+            height: -1,
+            type: "",
+            player: -1,
+            load: { type: "", player: -1 }
+        },
+        stage: ["Select a piece", ""]
     }),
 
     moves: {
-        clickSquare: (G, ctx, id) => {
-            //view totem pole
-            //if turn and if player == player, select piece
-        }
     },
 
-    turns: {
+    turn: {
+        onBegin: (G, ctx) => {
+            ctx.events.setStage('selectPiece');
+        },
         stages: {
+            examinePiece: {
+                moves: {
+                    clickSquare: (G, ctx, id) => {
+
+                    },
+                    clickPiece: (G, ctx, id) => {
+
+                    }
+                }
+            },
             selectPiece: {
                 moves: {
                     clickSquare: (G, ctx, id) => {
                         //view totem pole
-                        //if turn and if player == player, active piece = id
+                        //if turn and if player == player, active piece
+                        resetActivePiece(G);
                         let clickedSquare = G.squares[id];
                         G.activeSquare = id;
-                        if(clickedSquare.pole.length > 0 && clickedSquare.pole.last().player == ctx.currentPlayer) {
-                            G.activePiece = clickedSquare.pole.last();
-                        } else {
-                            G.activePiece.square = -1;
-                            G.activePiece.type = "";
-                            G.activePiece.player = -1;
+                        if (clickedSquare.pole.length > 0) {
+                            let clickedPiece = clickedSquare.pole[clickedSquare.pole.length - 1];
+                            if (clickedPiece.player == ctx.currentPlayer) {
+                                setActivePiece(G, ctx, id, clickedSquare.pole.length - 1, clickedPiece.type);
+                            }
                         }
                     },
                     clickPiece: (G, ctx, id) => {
-                        //if player == player and (at top or is elephant), active piece = id
+                        //if player == player and (at top or is elephant), active piece
                         let clickedPiece = G.activeSquare.pole[id];
-                        if(clickedPiece.player == ctx.currentPlayer
-                            && (id == G.activeSquare.pole.length-1 || clickedPiece.type == "E")) {
-                            G.activePiece = clickedPiece;
+                        if (clickedPiece.player == ctx.currentPlayer
+                            && (clickedPiece.height == G.activeSquare.pole.length - 1
+                                || (
+                                    (clickedPiece.type == "E" || clickedPiece == "F")
+                                    && clickedPiece.height == G.activeSquare.pole.length - 2
+                                )
+                            )
+                        ) {
+                            let load = { type: "", player: -1 };
+                            if (clickedPiece.type == "E" && clickedPiece.height != G.activeSquare.pole.length) { //0,1,2,3,4
+                                load.type = G.activeSquare.pole[G.activeSquare.pole.length - 1].type;
+                                load.player = G.activeSquare.pole[G.activeSquare.pole.length - 1].player;
+                            }
+                            setActivePiece(G, ctx, G.activeSquare, clickedPiece.height, clickedPiece.type, load);
                         } else {
-                            G.activePiece.square = -1;
-                            G.activePiece.type = "";
-                            G.activePiece.player = -1;
+                            resetActivePiece(G);
                         }
                     },
-                    confirmPiece: (G, ctx) => {
+                    confirm: (G, ctx) => {
+                        markValidMoves(G, ctx);
                         ctx.events.setStage('movePiece');
                         G.stage = "Move piece";
                         return;
@@ -79,19 +253,54 @@ export const Totems = {
             },
             movePiece: {
                 moves: {
+                    loadPiece: (G, ctx) => {
+                        if (G.activePiece.type == "F") {
+                            G.stage = "Select a piece to carry";
+                            ctx.events.setStage('carry');
+                        } else if (G.activePiece.type == "W") {
+                            load.type = G.activeSquare.pole[G.activeSquare.pole.length].type;
+                            load.player = G.activeSquare.pole[G.activeSquare.pole.length - 1].player;
+                        } else {
+                            //invalid move
+                        }
+                    },
                     clickSquare: (G, ctx, id) => {
                         let clickedSquare = G.squares[id];
+                        if (clickedSquare.valid) {
+                            G.activeSquare = id;
+                        }
                     },
-                    clickPiece: (G, ctx, id) => {
-                        let clickedSquare
+                    cancel: (G, ctx) => {
+                        resetValidMoves(G);
+                        G.stage = "Select a piece";
+                        ctx.events.setStage('selectPiece');
                     },
-                    submitMoves: (G, ctx) => {
+                    confirm: (G, ctx) => {
+                        if (G.activePiece.type == "W") {
+                            let load = { player: G.activePiece.load.player, type: G.activePiece.load.type };
+                            if (load.player >= 0) {
+                                G.squares[G.activeSquare].pole.push(load);
+                            }
+                        }
+                        G.squares[G.activeSquare].pole.push({ type: G.activePiece.type, player: G.activePiece.player });
+                        if (G.activePiece.type == "E") {
+                            let load = { player: G.activePiece.load.player, type: G.activePiece.load.type };
+                            if (load.player >= 0) {
+                                G.squares[G.activeSquare].pole.push(load);
+                            }
+                        }
+                        resetActivePiece(G);
+                        resetValidMoves(G);
+                        ctx.events.endTurn();
+                    }
+                }
+            },
+            carry: {
+                moves: {
+                    clickSquare: {
 
                     },
-                    resetMoves: (G, ctx) => {
-
-                    },
-                    undoMove: (G, ctx) => {
+                    confirmPiece: {
 
                     }
                 }
